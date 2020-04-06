274cf6f02adcffadb6ff227be5981bcc7e96cc9a
johannesberger <xaaris@googlemail.com>
2020-04-06 14:00:52 +0200

    Changed to second order (6D) kalman filter including acceleration

--- model/KalmanTracker.py
+++ model/KalmanTracker.py
@@ -1,33 +1,35 @@
 import numpy as np
-from filterpy.common import Q_discrete_white_noise, block_diag
+from filterpy.common import Q_discrete_white_noise
 from filterpy.kalman import KalmanFilter
 
 
 class KalmanTracker:
 
     def __init__(self, initial_pos_x=0, initial_pos_y=0):
-        self.kf = KalmanFilter(dim_x=4, dim_z=2)
+        self.kf = KalmanFilter(dim_x=6, dim_z=2)
 
-        self.kf.x = np.array([[initial_pos_x, initial_pos_y, 0., 0.]], np.float32).T
+        self.kf.x = np.array([[initial_pos_x, initial_pos_y, 0., 0., 0., 0.]], np.float32).T
 
-        dt = 1.0
-        self.kf.F = np.array([[1., 0., dt, 0.],  # pos.x, pos.y, vel.x, vel.y
-                              [0., 1., 0., dt],
-                              [0., 0., 1., 0.],
-                              [0., 0., 0., 1.]])  # state transition matrix
+        dt = 1.
+        self.kf.F = np.array([[1., 0., dt, 0., dt ** 2 / 2, 0.],  # pos.x, pos.y, vel.x, vel.y, acc.x, acc.y
+                              [0., 1., 0., dt, 0., dt ** 2 / 2],
+                              [0., 0., 1., 0., dt, 0.],
+                              [0., 0., 0., 1., 0., dt],
+                              [0., 0., 0., 0., 1., 0.],
+                              [0., 0., 0., 0., 0., 1.]])  # state transition matrix
 
-        self.kf.H = np.array([[1., 0, 0, 0],  # pos.x, pos.y, vel.x, vel.y
-                              [0., 1, 0, 0]])  # Measurement function
+        self.kf.H = np.array([[1., 0, 0, 0, 0, 0],  # pos.x, pos.y, vel.x, vel.y, acc.x, acc.y
+                              [0., 1, 0, 0, 0, 0]])  # Measurement function
 
-        self.kf.P *= 500.  # covariance matrix
-        self.kf.R = np.eye(2) * 100  # measurement uncertainty
-        q = Q_discrete_white_noise(2, dt=dt, var=.5, order_by_dim=False)
-        self.kf.Q = block_diag(q, q)  # process uncertainty
+        self.kf.P *= 1000.  # covariance matrix
+        self.kf.R = np.eye(2) * 50  # measurement uncertainty
+        self.kf.Q = Q_discrete_white_noise(2, dt=dt, var=.1, block_size=3, order_by_dim=False)  # process uncertainty
 
 
 
     def update(self, center_or_none):
-        self.kf.update(center_or_none)
+        if center_or_none:
+            self.kf.update(center_or_none)
         self.kf.predict()
 
     def predict_next_position(self):
@@ -36,6 +38,6 @@ class KalmanTracker:
 
     def get_uncertainty(self):
         """Returns a tuple with uncertainty in x and y direction"""
-        covariance_matrix = self.kf.S
-        covariances = np.diag(covariance_matrix)  # pos.x, pos.y, vel.x, vel.y
+        covariance_matrix = self.kf.P
+        covariances = np.diag(covariance_matrix)  # pos.x, pos.y, vel.x, vel.y, acc.x, acc.y
         return covariances[0], covariances[1]
