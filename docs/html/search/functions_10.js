bb1f38728caf45c53bcde5b05fc0f77b87c80612
johannesberger <xaaris@googlemail.com>
2020-10-28 00:24:21 +0100

    Updated documentation

--- docs/html/search/functions_10.js
+++ docs/html/search/functions_10.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['take_5fcenter_5fsquare_293',['take_center_square',['../namespaceutils_1_1image__utils.html#aa351e5375dd1062ca21ac908eadb7ec9',1,'utils::image_utils']]],
   ['timing_294',['timing',['../namespaceutils_1_1timer.html#accdf7af7013cba02094d413c0213c5f8',1,'utils::timer']]],
-  ['trim_5fzeros_5fgraph_295',['trim_zeros_graph',['../namespacemrcnn_1_1model.html#ad4bfbda49280eec8c42a478d0a2eddca',1,'mrcnn::model']]]
+  ['trim_5fzeros_5fgraph_295',['trim_zeros_graph',['../namespacemrcnn_1_1model.html#a6e77814e3c25de6941638195f8b2467f',1,'mrcnn::model']]]
 ];
